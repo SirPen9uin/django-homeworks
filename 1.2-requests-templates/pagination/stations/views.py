33bcee6a391a0d6c7fd1bfe74942bb241399c9be
Dmitrii Kovalev <sirpen9uin@gmail.com>
2023-12-16 15:06:16 +0300

    dj-hw-2, paginator - some fixes

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.core.paginator import Paginator
 from csv import DictReader
-
-
+from pagination import settings
 def index(request):
     return redirect(reverse('bus_stations'))
 
@@ -11,9 +10,7 @@ content = []
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-    with open(
-            r'C:\Users\riddi\PycharmProjects\dj-home\dj-homeworks\1.2-requests-templates\pagination\data-398-2018-08-30.csv',
-            newline='', encoding='utf-8') as f:
+    with open(settings.BUS_STATION_CSV, 'r' ,newline='', encoding='utf-8') as f:
         bus_stations = list(DictReader(f))
         paginator = Paginator(bus_stations, 10)
         page_number = int(request.GET.get('page', 1))
