6b91535d972a22bf6dcae7b5fe0aa10b6541ccbe
Dmitrii Kovalev <sirpen9uin@gmail.com>
2024-01-23 15:09:23 +0300

    Сериализатор - валидация

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,6 +1,11 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+
+from .permissions import IsOwnerOrReadOnly
+
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
@@ -8,8 +13,12 @@ class AdvertisementViewSet(ModelViewSet):
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
 
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create", "update", "partial_update", "delete"]:
             return [IsAuthenticated()]
         return []
