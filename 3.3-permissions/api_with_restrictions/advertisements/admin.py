5c72b1fec6b00b96b75483d6d0f28704a46065a0
Dmitrii Kovalev <sirpen9uin@gmail.com>
2024-01-24 22:46:11 +0300

    admin done

--- 3.3-permissions/api_with_restrictions/advertisements/admin.py
+++ 3.3-permissions/api_with_restrictions/advertisements/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Advertisement
 # Register your models here.
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'creator', 'created_at', 'updated_at')
+
+    actions = ['PATCH', 'DELETE']
+
+    def PATCH(self, request, queryset):
+        queryset.update(status='CLOSED' if queryset.filter(status='OPEN').exists() else 'OPEN')
+
+    def DELETE(self, request, queryset):
+        queryset.delete()
